ed2bcd82830e0a30c11eb50c9e9fb456b207daff
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-18 16:31:35 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, reset } from './redux/actions/counterActions';
 
 function App() {
+  const dispatch = useDispatch();
+  const count  = useSelector((state)=> state.count)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Count: {count} </h1>
+      <button onClick={()=>dispatch(increment())}>Increase</button>
+      <button onClick={()=>dispatch(decrement())}>Decrease</button>
+      <button onClick={()=>dispatch(reset())}>Reset</button>
+     
     </div>
   );
 }
---
+++ src/redux/reducers/counterReducer.js
@@ -0,0 +1,26 @@
+import { DECREMENT, INCREMENT, RESET } from "../constants/counterConstants";
+
+const initialState = {
+  count: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        count: state.count + -1,
+      };
+    case RESET:
+      return {
+        count: 0,
+      };
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
