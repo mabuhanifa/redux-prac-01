f506b69f8b29a97fd51e536e40f97cb44b72bab7
shourovahmd <moh.abuhanifa@gmail.com>
2022-07-19 22:33:16 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import Todos from "./components/todos/Todos";
 
 function App() {
-  
   return (
-    // <div >
-    //   <h1>Count: {count} </h1>
-    //   <button onClick={()=>dispatch(increment())}>Increase</button>
-    //   <button onClick={()=>dispatch(decrement())}>Decrease</button>
-    //   <button onClick={()=>dispatch(reset())}>Reset</button>
-
-    // </div>
     <div>
       <Todos />
     </div>
